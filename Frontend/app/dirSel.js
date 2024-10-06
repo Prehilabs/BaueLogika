45276c9f2626ab6befd71c6751a9b7ceab6d6b64
DIRM2705 <dirm2705@gmail.com>
2024-10-06 10:41:39 -0600

    Add window for problem selection

--- Frontend/app/dirSel.js
+++ Frontend/app/dirSel.js
@@ -1,5 +1,20 @@
 const {invoke} = window.__TAURI__.tauri;
 
 function getDir() {
-  invoke("choose_directory");
+  invoke("choose_directory").then((dir) => {
+    const problem_menu = document.getElementById("problem-menu");
+    const card = makeProblemCard("Problem 1");
+    problem_menu.innerHTML = card;
+  });
+}
+
+function makeProblemCard(problem){
+  const html = `
+  <div class="problem-card">
+    <h3>${problem}</h3>
+    <p>Problem Description</p>
+    <button>View</button>
+  </div>
+  `;
+  return html;
 }
--- src-tauri/src/commands/io.rs
+++ src-tauri/src/commands/io.rs
@@ -1,8 +1,10 @@
 use tauri::api::dialog::FileDialogBuilder;
+use tauri::{AppHandle, Manager};
 use crate::core::app_config::update_problem_path;
 
 #[tauri::command]
-pub async fn choose_directory(app_handle : tauri::AppHandle) {
+pub fn choose_directory(app_handle : AppHandle){
+    let app_handle_clone = app_handle.clone();
     FileDialogBuilder::new().pick_folder(move |sel_path|{
         //User selected a directory
         if sel_path.is_some() 
@@ -10,6 +12,10 @@ pub async fn choose_directory(app_handle : tauri::AppHandle) {
             let selected_path = sel_path.unwrap();
             let selected_path_str = selected_path.to_str().unwrap().to_string();
             update_problem_path(app_handle, selected_path_str);
+            let window = app_handle_clone.get_window("main");
+            if window.is_some(){
+                let cmd = window.unwrap().eval("window.location.href = 'problemSel.html';");
+            }
         }
     });
 }
